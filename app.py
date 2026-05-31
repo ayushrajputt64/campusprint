8c963d4c73df6513f6714a45f8ea945467632c57
Ayush anurag <ayushanurag001@gmail.com>
2026-05-31 07:09:24 +0530

    Fix Railway deployment

--- app.py
+++ app.py
@@ -207,4 +207,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print('Database ready!')
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
